3dd4bea4f5efa80a9ce4a1e23ee52a1bea79e60d
loqoella <qin775251410@gmail.com>
2019-10-18 21:21:12 +1100

    all front-end pages are pushed; all related URL and View are added and linked;

--- venv/src/minimiApp/urls.py
+++ venv/src/minimiApp/urls.py
@@ -2,7 +2,20 @@ from django.urls import path
 from .views import (
     home_view, 
     email_view,
-    about_view
+    about_view,
+    auburn_view,
+    campsie_view,
+    events_view,
+    event1_view,
+    event2_view,
+    event3_view,
+    curriculum_view,
+    curriculum1_view,
+    curriculum2_view,
+    curriculum3_view,
+    flexible_view,
+    enrolment_view,
+
     # SendEmail,
 )
 # from django.conf.urls import include, url
@@ -10,8 +23,20 @@ from .views import (
 app_name = 'products'
 urlpatterns = [
     path('', home_view, name='home'),
-
     path('about', about_view, name='about' ),
+    path('auburn', auburn_view, name='auburn' ),
+    path('campsie', campsie_view, name='campsie' ),
+    path('events', events_view, name='events' ),
+    path('event1', event1_view, name='event1' ),
+    path('event2', event2_view, name='event2' ),
+    path('event3', event3_view, name='event3' ),
+    path('curriculum', curriculum_view, name='curriculum' ),
+    path('curriculum1', curriculum1_view, name='curriculum1' ),
+    path('curriculum2', curriculum2_view, name='curriculum2' ),
+    path('curriculum3', curriculum3_view, name='curriculum3' ),
+    path('flexible', flexible_view, name='flexible' ),
+    path('enrolment', enrolment_view, name='enrolment' ),
+
 
 
     # for frontend email-sending
--- venv/src/minimiApp/views.py
+++ venv/src/minimiApp/views.py
@@ -13,20 +13,74 @@ def home_view(request, *args, **kwargs): # *args, **kwargs
     return render(request, "home.html", {})
 
 
-def about_view(request, *args, **kwargs):  # *args, **kwargs
+def about_view(request, *args, **kwargs): 
     print(args, kwargs)
     print(request.user)
-
-    # return HttpResponse("<h1>Hello World</h1>") # string of HTML code
     return render(request, "about.html", {})
 
 
-#def about_view(request, *args, **kwargs): # *args, **kwargs
- #   print(args, kwargs)
-  # print(request.user)
-   # return render(request, "about.html", {})
-    
-    
+def auburn_view(request, *args, **kwargs): 
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "auburn.html", {})
+
+
+def campsie_view(request, *args, **kwargs):  
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "campsie.html", {})
+
+
+def events_view(request, *args, **kwargs): 
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "events.html", {})
+
+def event1_view(request, *args, **kwargs):  
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "event1.html", {})
+
+def event2_view(request, *args, **kwargs): 
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "event2.html", {})
+
+def event3_view(request, *args, **kwargs):  
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "event3.html", {})
+
+def curriculum_view(request, *args, **kwargs): 
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "curriculum.html", {})
+
+def curriculum1_view(request, *args, **kwargs): 
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "curriculum1.html", {})
+
+def curriculum2_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "curriculum2.html", {})
+
+def curriculum3_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "curriculum3.html", {})
+
+def flexible_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "flexible.html", {})
+
+def enrolment_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "enrolment.html", {})
+   
 
 # # for frontend email-sending
 # def email_view(request, *args, **kwargs): # *args, **kwargs
